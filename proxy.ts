ac06e4706a76f5c605728406be68c29a74c3bc61
Paulo16548 <paulo16548@edu.barramansa.rj.gov.br>
2026-05-18 16:19:34 -0300

    fix auth proxy

--- proxy.ts
+++ proxy.ts
@@ -1,54 +1,5 @@
 import { NextResponse } from 'next/server'
-import type { NextRequest } from 'next/server'
 
-export function proxy(request: NextRequest) {
-
-  const pathname = request.nextUrl.pathname
-
-  // libera login
-  if (pathname === '/login') {
-    return NextResponse.next()
-  }
-
-  // procura qualquer cookie do supabase
-  const hasSupabaseCookie = request.cookies
-    .getAll()
-    .some(cookie => cookie.name.startsWith('sb-'))
-
-  // rotas protegidas
-  const protectedRoutes = [
-    '/admin',
-    '/alunos',
-    '/professores',
-    '/escolas',
-    '/atendimentos',
-    '/responsaveis',
-    '/cuidadores',
-    '/horarios',
-    '/relatorios'
-  ]
-
-  const isProtected = protectedRoutes.some(route =>
-    pathname.startsWith(route)
-  )
-
-  if (isProtected && !hasSupabaseCookie) {
-    return NextResponse.redirect(new URL('/login', request.url))
-  }
-
+export function proxy() {
   return NextResponse.next()
-}
-
-export const config = {
-  matcher: [
-    '/admin/:path*',
-    '/alunos/:path*',
-    '/professores/:path*',
-    '/escolas/:path*',
-    '/atendimentos/:path*',
-    '/responsaveis/:path*',
-    '/cuidadores/:path*',
-    '/horarios/:path*',
-    '/relatorios/:path*'
-  ]
 }
