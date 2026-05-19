cdd4cebbfc9320f45fb9222a2d970cbae3b82756
Paulo16548 <paulo16548@edu.barramansa.rj.gov.br>
2026-05-19 12:50:59 -0300

    disable authguard

--- components/AuthGuard.tsx
+++ components/AuthGuard.tsx
@@ -1,9 +1,53 @@
 'use client'
 
+import { useEffect, useState } from 'react'
+import { useRouter, usePathname } from 'next/navigation'
+import { supabase } from '@/lib/supabase'
+
+const rotasPublicas = ['/login']
+
 export default function AuthGuard({
   children,
 }: {
   children: React.ReactNode
 }) {
+  const router = useRouter()
+  const pathname = usePathname()
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const verificarSessao = async () => {
+      // rotas públicas
+      if (rotasPublicas.includes(pathname)) {
+        setLoading(false)
+        return
+      }
+
+      const {
+        data: { session },
+      } = await supabase.auth.getSession()
+
+      console.log('SESSION:', session)
+
+      if (!session) {
+        router.replace('/login')
+        return
+      }
+
+      setLoading(false)
+    }
+
+    verificarSessao()
+  }, [pathname, router])
+
+  if (loading) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        Carregando...
+      </div>
+    )
+  }
+
   return <>{children}</>
 }
